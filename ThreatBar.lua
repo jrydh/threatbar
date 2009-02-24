1ec727cd7a7b766185524e95e171c7ce2c0e1aa0
Johannes Rydh <jrydh@kth.se>
2009-02-24 14:28:21 +0100

    copyright and licence notices

--- ThreatBar.lua
+++ ThreatBar.lua
@@ -1,3 +1,21 @@
+--
+-- ThreatBar.lua
+-- Copyright 2008, 2009 Johannes Rydh
+--
+-- ThreatBar is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
 ThreatBar = LibStub("AceAddon-3.0"):NewAddon( "ThreatBar", "AceEvent-3.0" );
 
 function ThreatBar:OnInitialize()
